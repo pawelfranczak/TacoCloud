9d4a054b03cb942213b2819199a69d89b84c1a25
Pawel Franczak <gzem89@gmail.com>
2022-09-11 12:08:48 +0200

    User can create a Taco and Order

--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -3,24 +3,29 @@ package tacos.web;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Taco;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
 
+	@GetMapping
 	public String showDesignForm(Model model) {
 		
 		List<Ingredient> ingredients = Arrays.asList(
 				new Ingredient("FLTO", "pszenna", Type.WRAP),
+				new Ingredient("COTO", "kukurydziana", Type.WRAP),
 				new Ingredient("GRBF", "mielona wołowina", Type.PROTEIN),
 				new Ingredient("CARN", "kawałki mięsa", Type.PROTEIN),
 				new Ingredient("TMTO", "pomidory pokrojone w kostkę", Type.VEGGIES),
@@ -36,8 +41,16 @@ public class DesignTacoController {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
 		}
 		
+		model.addAttribute("design", new Taco());
+		
 		return("design");
 		
+	}
+	
+	@PostMapping
+	public String processDesign(Taco design) {
+		log.info("Przetwarzanie obiektu taco " + design);
+		return "redirect:/orders/current";
 	}
 	
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
