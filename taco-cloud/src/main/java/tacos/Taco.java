f80539501a8cafa485ddf7a1e18eee256a0d2b18
Pawel Franczak <gzem89@gmail.com>
2022-09-14 12:23:03 +0200

    Validation of Taco Design data.

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -1,13 +1,21 @@
 package tacos;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
 public class Taco {
-	
+	 
+	@NotNull
+	@Size(min = 5, message = "Nazwa musi składać się przynajmniej z 5 znaków")
 	private String name;
+	
+	@Size(min = 1, message = "Musisz wybrać przynajmniej jeden składnik")
 	private List<String> ingredients;
 
 }
--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -1,12 +1,17 @@
 package tacos.web;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,36 +24,44 @@ import tacos.Taco;
 @Controller
 @RequestMapping("/design")
 public class DesignTacoController {
-
-	@GetMapping
-	public String showDesignForm(Model model) {
-		
+	
+	@ModelAttribute
+	public void addIngredientsToModel(Model model) {
 		List<Ingredient> ingredients = Arrays.asList(
-				new Ingredient("FLTO", "pszenna", Type.WRAP),
-				new Ingredient("COTO", "kukurydziana", Type.WRAP),
-				new Ingredient("GRBF", "mielona wołowina", Type.PROTEIN),
-				new Ingredient("CARN", "kawałki mięsa", Type.PROTEIN),
-				new Ingredient("TMTO", "pomidory pokrojone w kostkę", Type.VEGGIES),
-				new Ingredient("LECT", "sałata", Type.VEGGIES),
-				new Ingredient("CHED", "chedar", Type.CHEESE),
-				new Ingredient("JACK", "Montery Jack", Type.CHEESE),
-				new Ingredient("SLSA", "pikantny sos pomidorowy", Type.SAUCE),
-				new Ingredient("SRCR", "smietana", Type.SAUCE)
+		  new Ingredient("FLTO", "pszenna", Type.WRAP),
+		  new Ingredient("COTO", "kukurydziana", Type.WRAP),
+		  new Ingredient("GRBF", "mielona wołowina", Type.PROTEIN),
+		  new Ingredient("CARN", "kawałki mięsa", Type.PROTEIN),
+		  new Ingredient("TMTO", "pomidory pokrojone w kostkę", Type.VEGGIES),
+		  new Ingredient("LETC", "sałata", Type.VEGGIES),
+		  new Ingredient("CHED", "cheddar", Type.CHEESE),
+		  new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+		  new Ingredient("SLSA", "pikantny sos pomidorowy", Type.SAUCE),
+		  new Ingredient("SRCR", "śmietana", Type.SAUCE)
 		);
 		
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
-			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+		  model.addAttribute(type.toString().toLowerCase(),
+		      filterByType(ingredients, type));
 		}
-		
-		model.addAttribute("design", new Taco());
-		
-		return("design");
+	}
+
+	@GetMapping
+	public String showDesignForm(Model model) {
+		Taco design = new Taco();
+		design.setIngredients(new ArrayList<String>());
+		model.addAttribute("design", design);
+		return "design";
 		
 	}
 	
 	@PostMapping
-	public String processDesign(Taco design) {
+	public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+		
 		log.info("Przetwarzanie obiektu taco " + design);
 		return "redirect:/orders/current";
 	}
