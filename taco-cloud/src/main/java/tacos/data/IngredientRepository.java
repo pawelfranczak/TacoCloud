759da51377ff1ef00d43759deeb5101cc9008ec0
Pawel Franczak <gzem89@gmail.com>
2022-10-26 15:24:15 +0200

    Make repository classes for working with JPA

--- taco-cloud/src/main/java/tacos/data/IngredientRepository.java
+++ taco-cloud/src/main/java/tacos/data/IngredientRepository.java
@@ -1,9 +1,11 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import tacos.Ingredient;
 
-public interface IngredientRepository {
-	Iterable<Ingredient> findAll();
-	Ingredient findById(String id);
-	Ingredient save(Ingredient ingredient);
+@Repository
+public interface IngredientRepository extends CrudRepository<Ingredient, String>{
+
 }
--- taco-cloud/src/main/java/tacos/data/OrderRepository.java
+++ taco-cloud/src/main/java/tacos/data/OrderRepository.java
@@ -1,9 +1,11 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import tacos.Order;
 
-public interface OrderRepository {
-
-	Order save(Order order);
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Long>{
 	
 }
