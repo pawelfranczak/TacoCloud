c5397129e160ea2c3232425e93eab5400837e9e7
Pawel Franczak <gzem89@gmail.com>
2023-02-13 14:26:06 +0100

    Add "takeIntoTotalSum" boolean field for Account. Handle this field on ui and backend.

---
+++ j2bills2/j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/controller/IndexController.java
@@ -0,0 +1,54 @@
+package pl.pfranczak.j2bills2.monolith.controller;
+
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import lombok.AllArgsConstructor;
+import pl.pfranczak.j2bills2.monolith.entity.Account;
+import pl.pfranczak.j2bills2.monolith.service.AccountService;
+import pl.pfranczak.j2bills2.monolith.service.UserService;
+import pl.pfranczak.j2bills2.monolith.service.UserSettingsService;
+import pl.pfranczak.j2bills2.monolith.service.bills.BillsOfMonthService;
+import pl.pfranczak.j2bills2.monolith.service.notification.NotificationService;
+
+@AllArgsConstructor
+@Controller
+public class IndexController {
+	
+	AccountService accountService;
+	UserService userService;
+	UserSettingsService userSettingsService;
+	NotificationService notificationService;
+	BillsOfMonthService billsOfMonthService;
+
+	@RequestMapping("${index}")
+	public ModelAndView index() {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth.isAuthenticated()) {
+			ModelAndView modelAndView = new ModelAndView("home");
+			if (userSettingsService.showAccountsSumOnHomepage()) {
+				modelAndView.addObject("sumOfAllAccounts", accountService.getSumOfAllActiveAndRelevantForSum());
+			}
+			userService.addUsernameToModelAndView(modelAndView);
+			
+			billsOfMonthService.payOutstandingAutomaticRepaymentBills();
+			notificationService.generateNotification();
+			
+			
+			List<Account> accounts = accountService.getAllActive();
+			modelAndView.addObject("accounts", accounts);
+			
+			long countOfActiveNotification = notificationService.getCountOfActiveNotification();
+			modelAndView.addObject("countOfActiveNotification", countOfActiveNotification+"");
+			return modelAndView;
+		}
+		return new ModelAndView("fail");
+	}
+	
+}
